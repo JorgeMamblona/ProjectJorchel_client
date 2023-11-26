75e6c1bda3cfe6cd21f204f473f14ce9ea00138b
RaquelWebDev <raquel.torre.oporto@gmail.com>
2023-11-26 20:47:22 +0100

    NAVBAR VERT SOLVED

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-const Login = () => {
-    return (
-        <h1>Soy el login</h1>
-=======
 import { Container, Row, Col } from "react-bootstrap"
 import LoginForm from "../components/LoginForm/LoginForm"
 import './Loginpage.css'
@@ -18,7 +13,6 @@ const Login = () => {
                 </Col>
             </Row>
         </Container>
->>>>>>> Stashed changes
     )
 }
 
