04f745ff17452435ac10f0dbe22569d1d9cb6449
Jormag <jorgemamblonagomez@gmail.com>
2023-11-27 09:18:58 +0100

    fix

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,9 +9,9 @@ const Login = () => {
                 <Col md={{ offset: 3, span: 6 }}>
                     <h1>Log in</h1>
                     <LoginForm />
-                </Col>
-            </Row>
-        </Container>
+                </Col >
+            </Row >
+        </Container >
     )
 }
 
