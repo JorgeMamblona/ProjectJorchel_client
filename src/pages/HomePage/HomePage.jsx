ba2b5f9334d37363ab466f3021255e910bbab756
RaquelWebDev <raquel.torre.oporto@gmail.com>
2023-12-06 16:31:08 +0100

    lottie

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,25 +1,48 @@
 import './HomePage.css'
+import React from 'react';
+import Lottie from 'lottie-react';
+
+import animationData from './../../lotties/Animation - 1701866819359.json'
+
 
 
 const HomePage = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
 
     return (
+        <div className="home-page">
+            <div>
+                <Lottie
+                    animationData={animationData}
+                    loop={true}
+                    autoplay={true}
+                    style={{ width: '50%', height: 'auto' }}
+                />
+            </div>
+            <div className="area" >
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div >
+        </div>
 
 
-        <div class="area" >
-            <ul class="circles">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div >
 
     )
 }
